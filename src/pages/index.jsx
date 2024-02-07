a5df83a24532386a734bb0db40f526755323bde0
mode-23 <ahmedbkd.258@gmail.com>
2024-02-07 12:05:32 +0100

    added env file

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -120,7 +120,7 @@ export default function Home() {
         onChange={(e) => setLoginPw(e.target.value)}
       />
       <button type="button" onClick={handleLogin}>
-        login
+        log in
       </button>
       <button
         type="button"
