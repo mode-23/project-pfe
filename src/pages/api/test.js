388c4cabd8f423475aa6b5c9a3fa48427bef5163
mode-23 <ahmedbkd.258@gmail.com>
2024-02-21 14:42:20 +0100

    added react calendar

--- src/pages/api/test.js
+++ src/pages/api/test.js
@@ -2,9 +2,15 @@ import { prisma } from "../../prisma_setup";
 
 const handler = async (req, res) => {
   const user = await prisma.user.findMany({
+    where: {
+      createdAt: {
+        gte: new Date("2024-02-07"), // Start of date range
+        lte: new Date("2024-02-20"), // End of date range
+      },
+    },
     orderBy: {
-      createdAt: 'desc'
-    }
+      createdAt: "desc",
+    },
   });
 
   res.status(200).json(user);
--- src/pages/projects/[name]/index.jsx
+++ src/pages/projects/[name]/index.jsx
@@ -1,11 +1,38 @@
 import { useRouter } from "next/router";
-import React from "react";
+import React, { useState } from "react";
 import styles from "./project.module.css";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+import { motion } from "framer-motion";
+import { PiCaretDoubleRightDuotone } from "react-icons/pi";
 
 const project = () => {
+  const [value, onChange] = useState(new Date());
+  const [value1, onChange1] = useState(new Date());
   const { query } = useRouter();
+  console.log(value);
 
-  return <div className={styles.project}>{query.name}</div>;
+  return (
+    <motion.div
+      className={styles.project}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 1 }}
+      key={query.name}
+    >
+      <div className={styles.projectHolder}>
+        <h4 className={styles.title}>
+          <PiCaretDoubleRightDuotone /> {query.name}
+        </h4>
+        <div className={styles.calendarsHolder}>
+          <Calendar onChange={onChange} value={value} />
+          <Calendar onChange={onChange1} value={value1} />
+        </div>
+        <button className={styles.orangeBtn}>Start</button>
+      </div>
+    </motion.div>
+  );
 };
 
 export default project;
